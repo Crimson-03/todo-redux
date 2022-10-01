49c4ac780e88b6f5e59821e449cb85b2680a1784
Krishan Kumar <92982687+Crimson-03@users.noreply.github.com>
2022-10-01 19:54:07 +0530

    Update index.js

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { ADDNEW_TODO, GETALL_TODO, TOGGLE_TODO, UPDATE_TODO, DELETE_TODO } from './type'
 
-const API_URL = 'http://localhost:8000'
+const API_URL = 'https://todo-redux-backend.herokuapp.com'
 
 export const addNewTodo = (data) => async (dispatch) => {
     try {
